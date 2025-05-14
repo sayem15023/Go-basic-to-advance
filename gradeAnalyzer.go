c5f36c4c840e0ce75d347a9da29c43e8c0ceb381
sayem chowdhury <sayem.chowdhury@technonext.com>
2025-05-14 17:31:01 +0600

    new nested code

---
+++ gradeAnalyzer.go
@@ -0,0 +1,10 @@
+package main
+
+import  "fmt"
+
+
+
+func analyzeScores(scores[]int) (avg float64,high int,low int){
+
+	
+}
---
+++ nestedarray.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	table := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("%d ", table[i][j])
+		}
+		fmt.Println()
+	}
+}
